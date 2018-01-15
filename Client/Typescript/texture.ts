ff81fcba4426c0485e4a9454c5d718e40e081dee
Frederic Dumont-Kristiansen <frederic.dumont.kristiansen@norkart.no>
2018-01-15 12:17:05 +0100

    better sun

--- Client/Typescript/texture.ts
+++ Client/Typescript/texture.ts
@@ -61,6 +61,7 @@ namespace Ocean
             this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR_MIPMAP_LINEAR);
             this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.REPEAT);
             this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.REPEAT);
+            
             this.gl.generateMipmap(this.gl.TEXTURE_2D);
         
             this.gl.texImage2D(this.gl.TEXTURE_2D, 0 , this.gl.RGBA32F, this.size, this.size, 0, this.gl.RGBA, this.gl.FLOAT, new Float32Array(dataArray));
